8b8230f2742eaa20355bbcbd316b8c5e1f38ace1
YashwantXDX <yashwantxdx@gmail.com>
2024-04-22 23:08:53 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/firebase_options.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
-import 'package:food_delivery_app/pages/select_mode_of_payment.dart';
 import 'package:food_delivery_app/splash/splash_screen.dart';
 import 'package:food_delivery_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
