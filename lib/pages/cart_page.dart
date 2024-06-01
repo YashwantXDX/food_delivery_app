f7c8b4d006d0a1d293a28b2b3e384bbfdeff78fc
YashwantXDX <yashwantxdx@gmail.com>
2024-06-01 12:45:07 +0530

    Initial Commit

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,15 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_cart_tile.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
-import 'package:food_delivery_app/pages/select_mode_of_payment.dart';
 import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class CartPage extends StatelessWidget {
+import 'delivery_in_progress_page.dart';
+
+
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
   @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+
+
+  late Razorpay _razorpay;
+
+  void openCheckout(amount) async{
+    amount =  amount * 100;
+    var options = {
+      'key' : 'rzp_test_1DP5mmOlF5G5ag',
+      'amount' : amount,
+      'name' : 'Yashwant Choure',
+      'prefill' : {'contact' : '8319785116', 'email' : 'yashwant.choure73546@gmail.com'},
+      'external' : {
+        'wallets' : ['paytm','phonepe']
+      }
+    };
+
+    try{
+      _razorpay.open(options);
+    }
+
+    catch(e){
+      debugPrint('Error : e');
+    }
+  }
+
+  void handlePaymentSuccess(PaymentSuccessResponse response) async{
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const DeliveryProgressPage(),));
+  }
+
+  void handlePaymentFailure(PaymentFailureResponse response){
+    Fluttertoast.showToast(msg: "Payment Failure " + response.message!);
+  }
+
+  void handleExternalWallet(ExternalWalletResponse response){
+    Fluttertoast.showToast(msg: "External Wallet" + response.walletName!,toastLength: Toast.LENGTH_SHORT);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlePaymentFailure);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleExternalWallet);
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+
     return Consumer<Restaurant>(builder: (context, restaurant, child) {
       // cart
       final userCart = restaurant.cart;
@@ -78,11 +141,7 @@ class CartPage extends StatelessWidget {
             MyButton(
                 onTap: () {
                   if(restaurant.getTotalPrice() != 0.0){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SelectModeOfPaymentPage(),
-                        ));
+                    openCheckout(restaurant.getTotalPrice());
                   }
                   else{
                     showDialog(context: context, builder: (context) => const AlertDialog(content: Text("Cart is Empty.."),),);
